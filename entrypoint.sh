e414fb33bb666ae7b5db205a64ef02446e1db8d3
Bartosz Olszewski <bartoszoloolszewski@gmail.com>
2024-03-18 12:14:57 +0100

    cmd refactor

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
-/opt/mssql/bin/sqlservr --accept-eula & sleep 30
 
+sleep 30 && \
 /opt/mssql-tools/bin/sqlcmd -S localhost -U SA -P ${SA_PASSWORD} -Q 'CREATE DATABASE [Minispace]' && \
-mvn liquibase:update
+mvn liquibase:update &
 
+/opt/mssql/bin/sqlservr --accept-eula 
